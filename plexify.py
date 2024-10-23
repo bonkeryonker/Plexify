5fbc19650d5388d0d3ee44a8869a2f9a68328278
gabu <gabeyonker@gmail.com>
2024-10-23 04:56:53 -0400

    Add files via upload

--- plexify.py
+++ plexify.py
@@ -3,6 +3,63 @@ import sys
 import re
 import shutil
 
+def digoutDirectories(_path, warnFileCount = 100):
+    """Recursively loops through all subdirectories and moves all files up to the folder denoted by _path.
+        If totalFilecount >= warnFileCount, ask the user if they're sure they want to continue.
+    
+    TODO:
+        Rename files that may be duplicate named so shutil.move works
+        Clean up all directories
+
+    Returns:
+        bool: True if the user did not abort.
+    """
+    totalFileCount = 0
+    totalDirCount = 0
+    fileList = []
+    for root, dirs, files in os.walk(_path):
+        totalDirCount += len(dirs)
+        for file in files:
+            totalFileCount += 1
+            fileList.append( (root, file) )
+    
+    if totalFileCount >= warnFileCount: #Ask user for confirmation if lots of files
+        print("This operation will move %d files into the %s directory, and delete %d source directories.\nAre you sure you want to continue? (y/N)" % (totalFileCount, _path, totalDirCount))
+        while(True):
+            userin = input().upper()
+            if userin == "Y":
+                break
+            else:
+                return False
+    
+    #move files
+    duplicateFileCount = 0
+    for f in fileList:
+        sourcePath = f[0] + '/' + f[1]
+        destPath = _path + '/' + f[1]
+        if not os.path.exists(destPath):
+            shutil.move(sourcePath, destPath)
+        else:
+            duplicateFileCount += 1
+            duplicateFileCountStr = str(duplicateFileCount)
+            shutil.move(sourcePath, _path + "/Copy (" + duplicateFileCountStr + ") - " + f[1])
+
+    #delete all directories and subdirectories
+    for dirpath, _, _ in os.walk(_path, topdown=False):
+        if dirpath == _path:
+            break
+        try:
+            os.rmdir(dirpath)
+        except OSError as ex:
+            print(ex)
+    
+    print("Moved %d files into %s. %d source directories deleted." % (totalFileCount, _path, totalDirCount))
+    return True
+    
+
+    
+
+
 def getFiles(_path):
     """Gets all files in passed directory.
 
@@ -54,12 +111,6 @@ def renameFiles(_fileList, _path):
 def makeSeasonDirs(_fileList, _path):
     """Counts how many unique seasons are in the list, and creates that many directories. Sorts files into their respective directories.
 
-    TODO:
-        Fix case where shutil.move will raise an exception if destination directory already has file with same name as source. 
-        (https://stackoverflow.com/questions/31813504/move-and-replace-if-same-file-name-already-exists)
-
-        Also, shutil.move simply calls os.rename in most cases. If unexpected errors occur, look into this as well.
-
     Returns:
         void
     """
@@ -73,11 +124,28 @@ def makeSeasonDirs(_fileList, _path):
 
         if not os.path.exists(currentSeasonPath):
             seasonCount += 1
-            print("Creating directory: %s" % currentSeasonPath)
+            #print("Creating directory: %s" % currentSeasonPath)
             os.makedirs(currentSeasonPath)
         movedCount += 1
         shutil.move(_path + '/' + f, currentSeasonPath + '/' + f)
-    print("%d files sorted into %d unique Seasons." % (movedCount, seasonCount))       
+    print("%d files sorted into %d unique Season directories." % (movedCount, seasonCount))       
+
+def promptAndCleanup(_path):
+    """Prompts the user if they'd like to remove all remaining files in the root directory.
+
+    Returns:
+        void
+    """
+    print("Would you like to delete all non-directory files in %s? (Y/n)" % _path)
+    while True:
+        userin = input().upper()
+        if userin == "N":
+            return
+        else:
+            break
+    for f in getFiles(_path):
+        os.remove(_path + '/' + f)
+    
     
      
 if __name__ == "__main__":
@@ -86,9 +154,14 @@ if __name__ == "__main__":
         path = sys.argv[1]
         extension = sys.argv[2]
 
-        print("Looking in %s for *%s" % (path, extension))
+        if not digoutDirectories(path):
+            print("Aborted.")
+            exit()
+        
         fileList = trimFiles(getFiles(path), extension)
         fileList = renameFiles(fileList, path)
         makeSeasonDirs(fileList, path)
+        promptAndCleanup(path)
+            
     else:
         print("Two arguments expected.\n\t(Example: plexify.py /path/to/file .fileExtension)")
